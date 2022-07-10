a63ffa0d784d289922872b4a4895ba18ba88ee8a
rishabhd2w <Rishabhchouhan.ad.co@gmail.com>
2022-07-10 14:39:01 +0530

    App Deployed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import SearchMovies from "./searchMovies";
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h4>Project Made By: Rishabh Singh</h4>
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies/>
+      </div>
+    );
+  }
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Main />, document.getElementById('root'));
